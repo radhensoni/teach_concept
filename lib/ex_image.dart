11320cc1c13d5c6e3536109bb009eba8f1c0cd9b
Radhen <aflutterdeveloper@gmail.com>
2023-10-25 23:55:02 +0530

    add internet permission

--- lib/ex_image.dart
+++ lib/ex_image.dart
@@ -11,17 +11,20 @@ class _EXImageState extends State<EXImage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("Example Image"),
-        ),
-        body: Center(
-          child: Container(
-            margin: const EdgeInsets.all(20),
-            child: const Image(
-              image: NetworkImage('https://www.tutorialkart.com/img/lion.jpg'),
-            ),
+      appBar: AppBar(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
+        title: Text("Example Image"),
+      ),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          child: const Image(
+            image: NetworkImage('https://www.tutorialkart.com/img/lion.jpg'),
           ),
-        ));
+        ),
+      ),);
   }
 }
