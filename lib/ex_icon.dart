06b74978983e3c0a5353c624da69ec41d33bcfce
Radhen <aflutterdeveloper@gmail.com>
2023-10-26 22:29:56 +0530

    changed format

--- lib/ex_icon.dart
+++ lib/ex_icon.dart
@@ -11,37 +11,38 @@ class _EXIconState extends State<EXIcon> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("Example Icon"),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            //basic example
-            Center(child: Icon(Icons.directions_transit)),
-            //change color of icon
-            Center(
-                child: Icon(
-              Icons.directions_transit,
-              color: Colors.green,
-              size: 70,
-            )),
-            //  Iconbutton
-            Container(
-              padding: EdgeInsets.all(50),
-              alignment: Alignment.center,
-              child: IconButton(
-                icon: Icon(
-                  Icons.directions_transit,
-                ),
-                iconSize: 50,
-                color: Colors.green,
-                splashColor: Colors.purple,
-                onPressed: () {},
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Example Icon"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          //basic example
+          Center(child: Icon(Icons.directions_transit)),
+          //change color of icon
+          Center(
+              child: Icon(
+            Icons.directions_transit,
+            color: Colors.green,
+            size: 70,
+          )),
+          //  Iconbutton
+          Container(
+            padding: EdgeInsets.all(50),
+            alignment: Alignment.center,
+            child: IconButton(
+              icon: Icon(
+                Icons.directions_transit,
               ),
+              iconSize: 50,
+              color: Colors.green,
+              splashColor: Colors.purple,
+              onPressed: () {},
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
